26fa7e3d62f77619990639dcd4adfe6b5c24e105
Piotr <ptkpospiech@gmail.com>
2019-06-12 18:38:26 +0200

    added repository module builder in PokemonModelComponent

--- app/src/main/java/com/example/pokefinder/di/component/PokemonModelComponent.kt
+++ app/src/main/java/com/example/pokefinder/di/component/PokemonModelComponent.kt
@@ -1,12 +1,13 @@
 package com.example.pokefinder.di.component
 
 import com.example.pokefinder.di.module.ApiModule
+import com.example.pokefinder.di.module.RepositoryModule
 import com.example.pokefinder.model.PokemonModel
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [ApiModule::class])
+@Component(modules = [ApiModule::class, RepositoryModule::class])
 interface PokemonModelComponent {
 
     fun inject(pokemonModel: PokemonModel)
@@ -16,5 +17,7 @@ interface PokemonModelComponent {
         fun build(): PokemonModelComponent
 
         fun apiModule(apiModule: ApiModule): Builder
+
+        fun repositoryModule(repositoryModule: RepositoryModule): Builder
     }
 }
