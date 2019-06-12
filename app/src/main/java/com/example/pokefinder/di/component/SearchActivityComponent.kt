76c24816dfb07a0716a8858edd8166d792b50de2
Piotr <ptkpospiech@gmail.com>
2019-06-12 20:25:10 +0200

    rebuilt search activity component to use module

--- app/src/main/java/com/example/pokefinder/di/component/SearchActivityComponent.kt
+++ app/src/main/java/com/example/pokefinder/di/component/SearchActivityComponent.kt
@@ -1,16 +1,22 @@
 package com.example.pokefinder.di.component
 
+import com.example.pokefinder.di.module.SearchPresenterModule
 import com.example.pokefinder.presenter.SearchPresenter
 import com.example.pokefinder.view.SearchActivity
 import dagger.Component
+import javax.inject.Singleton
 
-
-@Component
+@Singleton
+@Component(modules = [SearchPresenterModule::class])
 interface SearchActivityComponent {
-
-    fun getSearchPresenter(): SearchPresenter
 
     fun inject(searchActivity: SearchActivity)
 
-}
+    @Component.Builder
+    interface Builder {
+        fun build(): SearchActivityComponent
 
+        fun presenterBuilder(searchPresenterModule: SearchPresenterModule): Builder
+    }
+
+}
